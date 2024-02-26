854e1d9fda7ae8c4b98802240bfb0aafba636691
Rhine <coding.errorr@gmail.com>
2024-02-26 23:36:23 +0600

    📃 Doc : services page complete

--- lib/screens/servicescreen/wwdsection/wwdcard/what_we_deliver_card.dart
+++ lib/screens/servicescreen/wwdsection/wwdcard/what_we_deliver_card.dart
@@ -18,78 +18,64 @@ class WWDCard extends StatefulWidget {
 }
 
 class _WWDCardState extends State<WWDCard> {
-  late bool visible = false;
-
   @override
   Widget build(BuildContext context) {
-    return VisibilityDetector(
-        key: Key(widget.title),
-        onVisibilityChanged: (VisibilityInfo info) {
-          if (info.visibleFraction >= 0.5) {
-            setState(() {
-              visible = true;
-            });
-          } else {
-            visible = false;
-          }
-        },
-        child: Container(
-            width: MediaQuery.of(context).size.width * 0.9,
-            height: 400,
-            child: Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(12.0),
-                  child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 3000),
-                      curve: Curves.easeOutCubic,
-                      width: MediaQuery.of(context).size.width * 0.9,
-                      height: 400,
-                      transform: Matrix4.identity()..scale(visible ? 1.2 : 1),
-                      child: Image.asset(
-                        widget.background,
-                        height: 400,
-                        width: MediaQuery.of(context).size.width * 0.9,
-                        fit: BoxFit.cover,
-                      )),
-                ),
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(12.0),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
+    return Container(
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: 400,
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 3000),
+                  curve: Curves.easeOutCubic,
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  height: 400,
+                  transform: Matrix4.identity()..scale(1.2),
+                  child: Image.asset(
+                    widget.background,
                     height: 400,
-                    decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.bottomCenter,
-                            end: Alignment.topCenter,
-                            colors: [Colors.black54, Colors.black12])),
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    fit: BoxFit.cover,
+                  )),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: 400,
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: [Colors.black54, Colors.black12])),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    widget.title,
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        widget.title,
-                        style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30),
-                      ),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      Text(
-                        widget.description,
-                        style:
-                            const TextStyle(color: Colors.white, fontSize: 20),
-                        textAlign: TextAlign.justify,
-                      ),
-                    ],
+                  const SizedBox(
+                    height: 40,
                   ),
-                )
-              ],
-            )));
+                  Text(
+                    widget.description,
+                    style: const TextStyle(color: Colors.white, fontSize: 20),
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
